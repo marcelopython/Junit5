34e973688c835b7aee9e80dca9b5d594792a4c1f
Marcelo <msr88957@gmail.com>
2024-08-14 22:07:01 -0300

    life cycle Junit

--- src/test/java/br/com/unit/SimpleMathTest.java
+++ src/test/java/br/com/unit/SimpleMathTest.java
@@ -1,8 +1,7 @@
 package br.com.unit;
 
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,24 +9,60 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("Test math operations SimpleMath")
 public class SimpleMathTest {
 
+
+//    Primeiro ser rodado
+//    setup()
+
+//    Segundo a ser rodado
+//    beforeEach()
+
+//    Terceiro a ser rodado e fodado em cada teste
+//  afterEach()
+
+//    Quero a ser rodado executado depois de todos serem dodados
+//    afterAll()
+
+    @BeforeAll
+    static void setup() {
+        System.out.println("Running @BeforeAll method!");
+    }
+
+    @AfterAll
+    static void cleanup() {
+        System.out.println("Running @AfterAll method!");
+    }
+
     // test[System Under Test]_[Condition or State Change]_[Expected Result]
     @Test
     @DisplayName("Test 6.2 + 2 = 8.2")
     void testSum_When_SixDotTowIsAddedByTwo_ShouldReturnEightDotTwo() {
+        System.out.println("Teste testSum_When_SixDotTowIsAddedByTwo_ShouldReturnEightDotTwo");
+        // AAA Arrange, Act, Assert
 
-        SimpleMath math = new SimpleMath();
-        Double actual = math.sum(6.2D, 2D);
-        double expected = 8.2D;
-        assertEquals(expected, actual, () -> "The testSum() did not produce expected result");
+        // Given (Onde prepara as variaveis o que vai ser testado) Arrange
+            SimpleMath math = new SimpleMath();
+            double firstNumber = 6.2D;
+            double secondNumber = 2D;
+
+        // When (O que vai ser testado) Act
+            Double actual = math.sum(firstNumber, secondNumber);
+
+        // Then (onde verifica e se o o experado, onde que fica o codigo de teste) Assert
+            double expected = 8.2D;
+            assertEquals(expected, actual, () -> "The testSum() did not produce expected result");
     }
 
+    @Disabled("TODO: we need")
     @Test
     void testDivision_When_firstNumberIsDividedByZero_ShouldThrowArithmeticException() {
+        System.out.println("Teste testDivision_When_firstNumberIsDividedByZero_ShouldThrowArithmeticException");
 
     }
 
     @Test
     void testSubtraction() {
+        System.out.println("Teste testSubtraction");
+
         SimpleMath math = new SimpleMath();
         double actual = math.subtraction(1.0D, 2.0D);
         double expected = -1.0D;
@@ -36,6 +71,8 @@ public class SimpleMathTest {
 
     @Test
     void testMultiplication() {
+        System.out.println("Teste testMultiplication");
+
         SimpleMath math = new SimpleMath();
         double actual = math.multiplication(2.0D, 2.0D);
         double expected = 4.0D;
@@ -44,6 +81,8 @@ public class SimpleMathTest {
 
     @Test
     void testDivision() {
+        System.out.println("Teste testDivision");
+
         SimpleMath math = new SimpleMath();
         double actual = math.division(2.0D, 2.0D);
         double expected = 1.0D;
@@ -52,6 +91,8 @@ public class SimpleMathTest {
 
     @Test
     void testMean() {
+        System.out.println("Teste testMean");
+
         SimpleMath math = new SimpleMath();
         double actual = math.mean(2.0D, 2.0D);
         double expected = 2.0D;
@@ -60,6 +101,8 @@ public class SimpleMathTest {
 
     @Test
     void testSquareRoot() {
+        System.out.println("Teste testSquareRoot");
+
         SimpleMath math = new SimpleMath();
         double actual = math.squareRoot(3.0D);
         double expected = 1.7320508075688772D;
